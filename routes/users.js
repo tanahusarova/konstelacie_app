cdfb99beaa1cbe17646641a9ab4c96cf171d3642
Tatiana Husárová <tatianahusarova@MacBook-Air-uzivatela-Tatiana-2.local>
2023-07-26 10:58:30 +0200

    first commit backend

---
+++ routes/users.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+const cors = require('cors');
+router.use(cors());
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/members/:searchParams', (req, res) => {
+  const searchParams = req.params.searchParams;
+  fetch(`http://www.jusoft.sk/konstelacie/test/load.php?${searchParams}`, {
+        mode: 'no-cors',
+        headers: {
+          'Accept': 'text/plain',
+          'Content-Type': 'text/plain'
+      }, 
+        }).then(r => {
+            console.log("som tu r "+ r);
+            r.text()
+            .then(t => {
+              console.log("som tu t "+ t);
+              res.send(t);
+            })
+            .catch(err => {
+            console.log('je tu chyba a n tejto to pada');
+            res.status(500);
+        })
+      })
+      .catch(err => {
+          console.log('je tu chyba a na tejto to pada');
+          res.status(500);
+        })  
+})
+
+
+router.post('/members/:searchParams', (req, res) => {
+  const searchParams = req.params.searchParams;
+  fetch(`http://www.jusoft.sk/konstelacie/test/save.php?${searchParams}`, {
+    method: 'POST',
+    mode: 'no-cors',
+    headers: {
+        'Accept': 'text/plain',
+        'Content-Type': 'text/plain'
+    },
+    body: `${req.body}`
+})
+  .then(response => {
+    console.log("preslo");
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).send(error);
+  })
+})
+
+module.exports = router;
